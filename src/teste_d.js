fda632ad14f46913aab42bfe35321871fd0157f3
Bruno Barreira <brunobarreirasilva@gmail.com>
2021-04-09 23:08:12 -0300

    refactor: remove comments from teste_d file

--- src/teste_d.js
+++ src/teste_d.js
@@ -26,45 +26,13 @@ function solution(rowsAndColumns, numberList) {
 
     const finalMatrix = Array(rowsAndColumns)
 
-    console.log("[FINAL MATRIX CONST]", finalMatrix)
-    console.log("[FINAL MATRIX LENGTH]", finalMatrix.length)
-    console.log("[NUMBER LIST]", numberList)
-    console.log("[NUMBER LIST LENGTH]", numberList.length)
-
-    /**
-     * [TOTAL OF ELEMENTS] / [NUMBER OF ROWS AND COLUMNS] = [NUMBER OF ELEMENTS PER LINE]
-     * 
-     * [10, 90, 100, 40]
-     *  0       2
-     *  
-     * [10, 165, 77, 90, 2, 3, 90, 88, 10]
-     *  0            3          6
-     * 
-     * [123, 44, 55, 90, 8, 7, 33, 55, 99, 99, 100, 12, 111, 34, 21, 12]
-     *   0               4              8                12
-     *  
-     * [90, 65, 44, 33, 4, 11, 33, 55, 66, 7, 22, 12, 34, 56, 67, 12, 88, 77, 345, 345, 890, 987, 87, 11, 1, 11, 34, 56, 32, 45]
-     *  0                   5                     10                      15                           20                     
-    */
-
-    // POINT THE [[[[ LINES ]]]] OF THE MATRIX
     for(let  i = 0; i < finalMatrix.length; i++) {
         let matrixLineWithElements = []
-        // for(let j = 0; j < (numberList.length/rowsAndColumns); j++) {
-        //     console.log(j)
-        //     matrixLineWithElements[j] = numberList[j]
-        // }
         for(let k = 0; k < (numberList.length/rowsAndColumns); k++) {
-            console.log("           [K+I*FINALMATRIX.LENGTH] ", k+i*finalMatrix.length)
             matrixLineWithElements[k] = numberList[k+i*finalMatrix.length]
         }
-        // console.log("[MATRIX LINE WITH ELEMENTS]", matrixLineWithElements)
         finalMatrix[i] = matrixLineWithElements
-        // console.log(`[MATRIX ${i}]`, finalMatrix[i])
     }
-
-    console.log("[YOU ARE OUSTIDE FOR LOOP]: ")
-    console.log("[FINAL MATRIX]", finalMatrix)
 
     return finalMatrix
 }
